8c999a791c2a7b7468134757c7f15dee619c6551
hokrc01 <hokrc01@gmail.com>
2023-01-03 00:31:46 -0500

    Add files via upload

--- category-builder.component.cy.ts
+++ category-builder.component.cy.ts
@@ -10,6 +10,7 @@ describe('COMPONENT:  CategoryBuilderComponent', () => {
     cy.viewport(1920, 1080);
     cy.mount(CategoryBuilderComponent, {
       componentProperties: {},
+      imports:[],
       providers: [{ provide: CategoryBuilderService, useValue: mockService }],
     }).then((component) => {
       // someVar = component.divisionTotal;
@@ -18,20 +19,15 @@ describe('COMPONENT:  CategoryBuilderComponent', () => {
   it('show something', () => {
     cy.get('[data-test="cbc-subtitle"]').should('contain', '0-5');
   });
-  it('should test click of cancel', () => {
-
-    cy.get('[data-test="cbc-cancel"]')
-      .click()
-      .then(() => {
-        mockService.change.subscribe((resp: boolean) => {
-          expect(resp).to.be.false;
-        });        
-      });
+  it('should test click of cancel', () => {    
+    cy.get('[data-test="cbc-cancel"]').click()
+    //has mockService.change.next been called
 
   });
 });
 
 /*  mocks */
+// really do not like this.  should be a spy?
 class MockCategoryBuilder extends CategoryBuilderService {
   constructor() {
     super();
